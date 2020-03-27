a8cb02fec78f4138ea8005eb3455ae3dd3bc06c8
Patrick Lühne <patrick@luehne.de>
2020-03-27 16:17:11 +0100

    Add local time shortcodes

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,90 @@ function toggleNavigation(event)
 $(
 	function()
 	{
+		const localTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		const localDateFormatter = new Intl.DateTimeFormat("en-us",
+			{
+				year: "numeric",
+				month: "long",
+				day: "numeric"
+			});
+		const localDayPeriodFormatter = new Intl.DateTimeFormat("en-us",
+			{
+				hour: "numeric",
+				hour12: true,
+				dayPeriod: "short"
+			});
+
+		$("time-range").each(
+			function(index, timeRange)
+			{
+				const timeStamps = $("local-time", timeRange);
+
+				if (timeStamps.length != 2)
+					return;
+
+				try
+				{
+					const fromTimeStamp = $(timeStamps[0]).attr("datetime");
+					const toTimeStamp = $(timeStamps[1]).attr("datetime");
+					const fromDateTime = Date.parse(fromTimeStamp);
+					const toDateTime = Date.parse(toTimeStamp);
+					const localFromDate = localDateFormatter.format(fromDateTime);
+					const localToDate = localDateFormatter.format(toDateTime);
+					const localFromDayPeriod = localDayPeriodFormatter.format(fromDateTime).slice(-2);
+					const localToDayPeriod = localDayPeriodFormatter.format(toDateTime).slice(-2);
+
+					let fromTimeFormat =
+					{
+						year: "numeric",
+						month: "long",
+						day: "numeric",
+						hour: "numeric",
+						minute: "numeric",
+						hour12: true,
+						dayPeriod: "short"
+					};
+					let toTimeFormat =
+					{
+						year: "numeric",
+						month: "long",
+						day: "numeric",
+						hour: "numeric",
+						minute: "numeric",
+						hour12: true,
+						dayPeriod: "short"
+					};
+
+					if (localFromDate == localToDate)
+					{
+						delete toTimeFormat.year;
+						delete toTimeFormat.month;
+						delete toTimeFormat.day;
+					}
+
+					const localFromDateTimeFormatter = new Intl.DateTimeFormat("en-us", fromTimeFormat);
+					const localToDateTimeFormatter = new Intl.DateTimeFormat("en-us", toTimeFormat);
+
+					let localFromDateTime = localFromDateTimeFormatter.format(fromDateTime);
+					let localToDateTime = localToDateTimeFormatter.format(toDateTime);
+
+					if (localFromDate == localToDate && localFromDayPeriod == localToDayPeriod)
+					{
+						localFromDateTime = localFromDateTime.slice(0, -3);
+					}
+
+					localFromDateTime = localFromDateTime.replace(" AM", " a.m.").replace(" PM", " p.m.");
+					localToDateTime = localToDateTime.replace(" AM", " a.m.").replace(" PM", " p.m.");
+
+					$(timeStamps[0]).html(localFromDateTime)
+					$(timeStamps[1]).html(localToDateTime + " [" + localTimeZone + "]")
+				}
+				catch (error)
+				{
+					console.warn(error);
+				}
+			});
+
 		$(".toggle-navigation").on("click",
 			function(event)
 			{
